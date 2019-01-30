8ca9a785b5f57e8859967b0229b4d111b1d0ed44
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-01-30 08:02:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 function destructivelyAppendKitten(Ralph){
   kittens.push("Ralph")
 }
+
+function destructivelyPrependKitten(name){
+  kittens.unshift("name")
+}
